2c4a5d9542ec20292af16b256b06b15d96a1b15f
jmorris <jmorris.geo@gmail.com>
2019-11-08 11:48:02 -0800

    added raster package

--- Tpoints.R
+++ Tpoints.R
@@ -2,7 +2,7 @@
 #by a query to provide catagories, creates randome points for training. creates a training point crv.
 
 #load packages
-require(c("bcdata", "dtidyverse", "sf", "mapView" ))
+require(c("bcdata", "dtidyverse", "sf", "mapView", "raster"))
 
 # set test variables
 layer = "e5bf92e9-3323-4eb6-b051-7fe89b5174a9"
