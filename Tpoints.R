4f312e04c1bb91d286b311041e8140e66226bddc
jmorris <jmorris.geo@gmail.com>
2019-11-08 11:08:47 -0800

    Added bcdc_query_geodata to pull BCLC_level1 from defined AOI

--- Tpoints.R
+++ Tpoints.R
@@ -1,2 +1,18 @@
 #accepts a polygon area of interests. Selects 
 #by a query to provide catagories, creates randome points for training. creates a training point crv.
+
+library(bcdata)
+library(tidyverse)
+library(mapview)
+library(sf) # this is for reading in the aoi .shp for initial testing
+
+# Here we are grabbing a bunch of data from bcdata to make a map of bc looking at fire and transmission lines
+
+aoifile <- "inputs/AOI/DeceptionProjectBoundary.shp"
+
+aoi <- read_sf(aoifile)
+
+tpoly <- bcdc_query_geodata('vri-forest-vegetation-composite-polygons-and-rank-1-layer') %>% #query_geodata will just query whats available, not acquire it
+  filter(INTERSECTS(aoi)) %>%
+  select(BCLCS_LEVEL_1) %>% # this will filter data from geodata BEFORE it sends it over, so we're only grabbing the 2017 data from the web
+collect() # collect() will collect the data that you've queried
