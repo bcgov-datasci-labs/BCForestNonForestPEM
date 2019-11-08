ebde2a7299dd184d3c9d873a5feeb8b92f453657
Firstname Lastname <firstname.lastname@gov.bc.ca>
2019-11-08 11:22:55 -0800

    setup

---
+++ header.R
@@ -0,0 +1,17 @@
+library(sf)
+library(dplyr)
+library(readr)
+library(raster)
+
+TmpDir <- 'tmp'
+OutDir <- 'out'
+DataDir <- 'data'
+dataOutDir <- file.path(OutDir,'data')
+tileOutDir <- file.path(dataOutDir,'tile')
+
+dir.create(TmpDir, showWarnings = FALSE)
+dir.create(OutDir, showWarnings = FALSE)
+dir.create(DataDir, showWarnings = FALSE)
+dir.create(file.path(dataOutDir), showWarnings = FALSE)
+dir.create(file.path(tileOutDir), showWarnings = FALSE)
+
