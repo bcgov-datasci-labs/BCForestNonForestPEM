12c4065b10066dcbe44dece4327b0e538dd3ea84
Firstname Lastname <firstname.lastname@gov.bc.ca>
2019-11-08 12:42:37 -0800

    dem10

--- 01_Load_DEM.R
+++ 01_Load_DEM.R
@@ -19,6 +19,7 @@ DEM.dir<-'inputs/DEM_derived'
 AOI.dir<-'inputs/AOI'
 Image.dir<-'data/imagery'
 DEM.out.dir<-'data'
+
 DEM25<-raster(file.path(DEM.dir,'MRVBF25m.tif'))
 
 AOI<-st_read(file.path(AOI.dir,'DeceptionProjectBoundary.shp'))
@@ -33,6 +34,10 @@ AOIRast<-raster(xmn=AOI.bbox[1], xmx=AOI.bbox[3],
 #DEM10<-raster::disaggregate(DEM25, fact=2.5)
 DEM10<-raster::resample(DEM25, AOIRast, method='ngb')
 writeRaster(DEM10,filename=file.path('./data/DEM10.tif'), format="GTiff", overwrite=TRUE)
+
+
+
+
 #code for reading from
 railways <- bcdc_get_data("railway-track-line",
                           resource = "bf30d34e-1f6b-4034-a35c-1cf7c9707ae7")
