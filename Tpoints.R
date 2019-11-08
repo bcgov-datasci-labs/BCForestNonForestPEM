a8c0143421e9d66d74a4a3a1b378acf648e8ef6e
dfilatow <deepa.filatow@gov.bc.ca>
2019-11-08 10:54:53 -0800

    load libraries and test variables

--- Tpoints.R
+++ Tpoints.R
@@ -1,2 +1,13 @@
 #accepts a polygon area of interests. Selects 
 #by a query to provide catagories, creates randome points for training. creates a training point crv.
+
+# set test variables
+layer = "e5bf92e9-3323-4eb6-b051-7fe89b5174a9"
+query = 
+
+#load pacakges
+require(c("bcdata", "dplyr", "sf"))
+
+
+
+#load BCDATA
